190995983b8af164bfdb4cac47c6f4c7d7a0bea4
arun-alagu <arunalagu8825@gmail.com>
2023-11-29 14:11:13 +0530

    updated collections

--- Practice/src/collections/Main.java
+++ Practice/src/collections/Main.java
@@ -1,22 +1,37 @@
 package collections;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Integer> list = List.of(1,2,3,4);
+
         Node<Integer> head = new Node<>(5);
         head.next = new Node<>(10);
         head.next.next = new Node<>(15);
 
-        NodeIterator<Integer> iterator = new NodeIterator<>(head);
+//        NodeIterator<Integer> iterator = new NodeIterator<>(head);
+//
+//        while(iterator.hasNext()){
+//            System.out.println(iterator.next().data);
+//        }
+//
+//        System.out.println("\n");
+//
+//        LinkedList<Integer> ll = new LinkedList<>(head);
+//        for(Node<Integer> i : ll){
+//            System.out.println(i.data);
+//        }
+        Iterator<Integer> iterator = list.iterator();
 
         while(iterator.hasNext()){
-            System.out.println(iterator.next().data);
+            System.out.printf("%d ",iterator.next());
         }
-
-        System.out.println("\n");
-
-        LinkedList<Integer> ll = new LinkedList<>(head);
-        for(Node<Integer> i : ll){
-            System.out.println(i.data);
+        System.out.println();
+        for (Integer integer : list) {
+            System.out.printf("%d ",integer);
         }
     }
 }
