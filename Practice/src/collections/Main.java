3d9d83b0552b1febd4a97fda587f8c32896fff38
arun-alagu <arunalagu8825@gmail.com>
2023-11-29 14:25:52 +0530

    added print statements

--- Practice/src/collections/Main.java
+++ Practice/src/collections/Main.java
@@ -1,35 +1,43 @@
 package collections;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> list = List.of(1,2,3,4);
+        List<Integer> list = new ArrayList<>(List.of(1,2,3));
 
         Node<Integer> head = new Node<>(5);
         head.next = new Node<>(10);
         head.next.next = new Node<>(15);
 
-//        NodeIterator<Integer> iterator = new NodeIterator<>(head);
-//
-//        while(iterator.hasNext()){
-//            System.out.println(iterator.next().data);
-//        }
-//
-//        System.out.println("\n");
-//
-//        LinkedList<Integer> ll = new LinkedList<>(head);
-//        for(Node<Integer> i : ll){
-//            System.out.println(i.data);
-//        }
+        NodeIterator<Integer> NodeIterator = new NodeIterator<>(head);
+
+        System.out.print("Linked List iteration using iterator: ");
+        while(NodeIterator.hasNext()){
+            System.out.printf("%d ",NodeIterator.next().data);
+        }
+
+        System.out.println();
+        System.out.print("Linked List iteration using enhanced for: ");
+        LinkedList<Integer> ll = new LinkedList<>(head);
+        for(Node<Integer> i : ll){
+            System.out.printf("%d ",i.data);
+        }
+
         Iterator<Integer> iterator = list.iterator();
 
+        System.out.println();
+        System.out.print("List iteration using iterator: ");
         while(iterator.hasNext()){
             System.out.printf("%d ",iterator.next());
         }
+
         System.out.println();
+
+        System.out.print("List iteration using enhanced for: ");
         for (Integer integer : list) {
             System.out.printf("%d ",integer);
         }
