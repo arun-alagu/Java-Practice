a3dddac70d26b05035cd48bff0fad68e1edb1203
arun-alagu <arunalagu8825@gmail.com>
2023-12-29 09:30:45 +0530

    simple check

--- Practice/src/Main.java
+++ Practice/src/Main.java
@@ -1,17 +1,61 @@
+import collections.LinkedList;
+import collections.Node;
+import collections.NodeIterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.Callable;
+
 public class Main {
-    public static void main(String[] args) {
-       String s = new String("Hello");
-       String t = new String("Hello");
+    static class Node implements  Iterable<Integer>{
+        private int data;
+        private Node next;
 
-       String copyS = s;
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
 
-        System.out.println("s == t: "+(s==t));
-        System.out.println("s == copyS: "+(s==copyS));
-        System.out.println("s.equals(t): "+ s.equals(t));
-        System.out.println("s.equals(copyS): "+s.equals(copyS));
-
-        System.out.println("Hello World!");
-
+        @Override
+        public Iterator<Integer> iterator() {
+            return new NodeIterator(this);
+        }
     }
+
+    static class NodeIterator implements Iterator<Integer> {
+
+        Node curr;
+
+        NodeIterator(Node curr){
+            this.curr = curr;
+        }
+
+        public boolean hasNext(){
+            return curr != null;
+        }
+
+        public Integer next(){
+            Integer toReturn = curr.data;
+            curr = curr.next;
+
+            return toReturn;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        collections.Node<Integer> head = new collections.Node<>(5);
+        head.next = new collections.Node<>(1);
+        head.next.next = new collections.Node<>(15);
+
+        collections.NodeIterator<Integer> NodeIterator = new collections.NodeIterator<>(head);
+
+        System.out.print("Linked List iteration using iterator: ");
+        while(NodeIterator.hasNext()){
+            System.out.printf("%d ",NodeIterator.next().data);
+        }
+    }
+
 
 }
---
+++ Practice/src/factoryDesign/PC.java
@@ -0,0 +1,2 @@
+package factoryDesign;public class PC {
+}
