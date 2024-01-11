49d98982f737c1406876e9604425afdb9ec0c047
arun-alagu <ahesharun@gmail.com>
2024-01-11 08:08:23 +0530

    singleton enum instance

--- Practice/src/singletonPattern/Client.java
+++ Practice/src/singletonPattern/Client.java
@@ -3,18 +3,23 @@ package singletonPattern;
 public class Client {
 
     public static void main(String[] args) {
-        EarlyInstantiation e1 = EarlyInstantiation.getInstance();
-        e1.hello();
+//        EarlyInstantiation obj1 = EarlyInstantiation.getInstance();
+//        LazyInstantiation obj1 = LazyInstantiation.getInstance();
+        EnumSingleton obj1 = EnumSingleton.INSTANCE;
+        obj1.hello();
         System.out.println("-------------------------------------------------");
-        System.out.println("Value display using object e1: "+e1.displayValue());
-        System.out.println("Value add using object e1: "+e1.addValue());
+        System.out.println("Value display using object obj1: "+obj1.displayValue());
+        System.out.println("Value add using object obj1: "+obj1.addValue());
         System.out.println("-------------------------------------------------");
-        EarlyInstantiation e2 = EarlyInstantiation.getInstance();
-        System.out.println("Value display using object e2: "+e2.displayValue());
-        System.out.println("Value add using object e2: "+e2.addValue());
+
+//        EarlyInstantiation obj2 = EarlyInstantiation.getInstance();
+//        LazyInstantiation obj2 = LazyInstantiation.getInstance();
+        EnumSingleton obj2 = EnumSingleton.INSTANCE;
+        System.out.println("Value display using object obj2: "+obj2.displayValue());
+        System.out.println("Value add using object obj2: "+obj2.addValue());
         System.out.println("-------------------------------------------------");
-        System.out.println("Value display using object e1: "+e1.displayValue());
-        System.out.println("Value display using object e2: "+e2.displayValue());
+        System.out.println("Value display using object obj1: "+obj1.displayValue());
+        System.out.println("Value display using object obj2: "+obj2.displayValue());
     }
 
 }
--- Practice/src/singletonPattern/EnumSingleton.java
+++ Practice/src/singletonPattern/EnumSingleton.java
@@ -1,2 +1,19 @@
-package singletonPattern;public enum EnumSingleton {
+package singletonPattern;
+
+public enum EnumSingleton {
+    INSTANCE;
+
+    private int value = 0;
+
+    public void hello() {
+        System.out.println("Hello from Enum Instantiation!");
+    }
+
+    public int addValue(){
+        return ++value;
+    }
+
+    public int displayValue(){
+        return value;
+    }
 }
